3a871b7c832c1916f999b771f667d96eaf6252a2
Kadyrbaev <sadyr_9494@mail.ru>
2024-09-04 16:51:41 +0600

    clean project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import './App.css';
-import Header from './layout/Header';
-import MainImageSlider from './layout/MainImageSlider';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <MainImageSlider/>
+      
     </div>
   );
 }
--- src/components/Test.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-export const Test = () => {
-  return (
-    <div>Test</div>
-  )
-}
-// secer-project
