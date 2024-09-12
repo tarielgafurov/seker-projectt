eaf93388ae897de0cf57d497c61b76c942c9d0e2
bektur <b00095468@gmail.com>
2024-09-12 15:17:47 +0600

    ModalStyle

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import './App.css';
+import React from 'react'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
+    <div>
+
     </div>
-  );
+  )
 }
-export default App;
+
+export default App
--- src/UI/Modal.jsx
+++ src/UI/Modal.jsx
@@ -2,78 +2,93 @@ import React from "react";
 import styled from "styled-components";
 const Modal = () => {
   return (
-    <StyleModal>
-      <StyleBox>
-        <StyleH1>Мой профиль</StyleH1>
+    <Bakdrop>
+      <StyleModal>
         <StyleBox>
-          <StyleInput type="text" placeholder="E-mail" />
-          <br />
-          <br />
-          <br />
-          <StyleInput type="text" placeholder="Телефон номер" />
-          <br />
-          <br />
-          <br />
-          <StyleInput type="password" placeholder="Пароль" />
-          <br />
-          <StyleButton2> Войти через Google</StyleButton2>
-          <StyleButton>Войти</StyleButton>
+          <StyleBox>
+          <StyleH4>Мой профиль</StyleH4>
+            <StyleInput type="text" placeholder="E-mail" />
+            <br />
+            <br />
+            <StyleInput type="text" placeholder="Телефон номер" />
+            <br />
+            <br />
+            <StyleInput type="password" placeholder="Пароль" />
+            <br />
+            <StyleButton2> Войти через Google</StyleButton2>
+            <StyleButton>Войти</StyleButton>
+          </StyleBox>
         </StyleBox>
-      </StyleBox>
-    </StyleModal>
+      </StyleModal>
+    </Bakdrop>   
   );
 };
 
 export default Modal;
+const Bakdrop = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  z-index: 10;
+`;
 
 const StyleModal = styled.div`
-  background-color: #fffdf0;
-  width: 400px;
-  height: 500px;
+padding: 5px;
+  background-color: #d9d9d9;
+  margin-top: 100px;
+  width: 390px;
+  height: 365px;
   border-radius: 20px;
-  position: fixed;
   box-shadow: rgba(50, 50, 93, 0.25) 0px 6px 12px -2px,
     rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;
   margin-left: 500px;
   text-align: center;
 `;
-const StyleBox = styled.div`
-  padding: 10px;
-  margin-top: 30px;
-`;
+const StyleBox = styled.div``;
+
 const StyleInput = styled.input`
-  background-color: #dae9f7;
+  background-color: #ffffff;
   border: none;
   outline: none;
-  height: 40px;
-  width: 290px;
+  height: 20px;
+  width: 270px;
   border-radius: 10px;
+  padding: 10px;
 `;
-const StyleButton = styled.button`
-  background-color: #fc3a74;
-  cursor: pointer;
-  height: 40px;
+const StyleButton2 = styled.button`
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 6px 12px -2px,
+    rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;
+  &:active {
+    background-color: #00ffff4c;
+  }
   border: none;
   border-radius: 10px;
   width: 290px;
-  margin-top: 13px;
-  &:active{
-     background-color: #fff0fc;
-  }
-`;
-const StyleH1 = styled.h1`
-  color: #fc3a74;
-`
-const StyleButton2 = styled.button`
-  box-shadow: rgba(50, 50, 93, 0.25) 0px 6px 12px -2px,
-  rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;
-  &:active{
-    background-color: #00ffff4c;
-  }
-border: none;
-  border-radius: 10px;
-  width: 290px;    
   height: 40px;
   margin-top: 15px;
   cursor: pointer;
+`;
+
+const StyleButton = styled.button`
+  background-color: #fc3a74;
+  color: #fff0fc;
+
+  cursor: pointer;
+  height: 40px;
+  border: none;
+  border-radius: 10px;
+  width: 290px;
+  margin-top: 18px;
+  &:active {
+    background-color: #fff0fc;
+  }
+`;
+const StyleH4 = styled.h3`
+  color: #fc3a74;
+
+
 `
+
